c68f801f4c0fddbcc3c3146596bf997e984267e8
Jiří Stránský <jistr@jistr.com>
2015-10-24 20:50:00 +0200

    Add debug mode and some status messages

--- download-sources.sh
+++ download-sources.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -x
 set -euo pipefail
 
 BIN_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
@@ -9,24 +8,36 @@ source "$BIN_DIR/lib/variables.sh"
 
 function download_firmware() {
     if [ ! -e "$FIRMWARE_PATH_COMPRESSED" -a ! -e "$FIRMWARE_PATH" ]; then
+        print_stderr "Downloading firmware..."
         curl -L -o "$FIRMWARE_PATH_COMPRESSED" "$RPI_FIRMWARE_URL"
     fi
 
     if [ ! -e "$FIRMWARE_PATH" ]; then
-        tar -C "$SOURCE_DIR/firmware" -xzvf "$FIRMWARE_PATH_COMPRESSED"
+        print_stderr "Extracting firmware..."
+        tar -C "$SOURCE_DIR/firmware" -xzf "$FIRMWARE_PATH_COMPRESSED"
         [ -e "$FIRMWARE_PATH" ] || exit_error "Firmware extracted but not found"
         rm "$FIRMWARE_PATH_COMPRESSED"
+    else
+        print_stderr "Firmware is downloaded and extracted."
     fi
 }
 
 function download_os_image() {
     if [ ! -e "$OS_IMAGE_PATH_COMPRESSED" -a ! -e "$OS_IMAGE_PATH" ]; then
+        print_stderr "Downloading OS image..."
         curl -L -o "$OS_IMAGE_PATH_COMPRESSED" "$RPI_OS_IMAGE_URL"
     fi
 
-    [ -e "$OS_IMAGE_PATH" ] || unxz "$OS_IMAGE_PATH_COMPRESSED"
+    if [ ! -e "$OS_IMAGE_PATH" ]; then
+        print_stderr "Extracting OS image..."
+        unxz "$OS_IMAGE_PATH_COMPRESSED"
+    else
+        print_stderr "OS image is downloaded and extracted."
+    fi
 }
 
+init
 make_resource_and_output_dirs
 download_os_image
 download_firmware
+exit_success
--- lib/common.sh
+++ lib/common.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+function init() {
+    if [ "${DEBUG:-0}" = "1" ]; then
+        set -x
+    fi
+}
+
+function exit_success() {
+    print_stderr "SUCCESS."
+    exit 0
+}
+
 function exit_error() {
-    echo "$@"
+    print_stderr "ERROR:" "$@"
     exit 1
+}
+
+function print_stderr() {
+    echo "$@" > /dev/fd/2
 }
 
 function make_resource_and_output_dirs() {
